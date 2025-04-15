058bea72adffc1f390f2f3b8b57eecfea633c48e
Lukasz Gandecki <lgandecki@css.edu>
2025-04-15 11:58:04 +0200

    tiny react cleanup

--- src/react-components.tsx
+++ src/react-components.tsx
@@ -2,12 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import BookChaptersModal from "./book-chapters-modal";
 
-const MyFirstComponent = () => {
-  return <BookChaptersModal />;
-};
-
 export const startReactComponents = () => {
   const root = createRoot(document.getElementById("chapters-root"));
-  root.render(<MyFirstComponent />);
+  root.render(<BookChaptersModal />);
 };
-
