d2a8f840098894bdd2802ada122aa5382b99a36c
Lukasz Gandecki <lgandecki@css.edu>
2025-04-30 17:27:04 +0200

    comments in editor watcher

--- editor_watcher.sh
+++ editor_watcher.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-fswatch -o src/data/chapters.xml | while read num; do
+fswatch -o src/data/chapters.xml | while read num; do ## ZMIENIAMY ZE WSZYSTKIE CHAPTERY
   echo "File changed, running scripts..."
-  bun src/data/xmlToComplexHtml.ts
-  bun src/data/tools/get-character-tags-per-xml.ts
+  bun src/data/xmlToComplexHtml.ts ## To generuje ./src/data/chapters.ts
+  bun src/data/tools/get-character-tags-per-xml.ts  ## To generuje ./src/data/metadata.ts
   echo "Scripts executed successfully."
 done
+
+
+## Efektem tego pliku sa te dwa pliku w data ktore odpowiadaja za informacje kto jest w ktorym paragrafie i tekst ksiazki
