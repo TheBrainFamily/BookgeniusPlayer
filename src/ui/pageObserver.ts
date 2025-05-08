7786d7df56f69a4dd7047180d71a3a610c501c8d
Lukasz Gandecki <lgandecki@css.edu>
2025-05-08 17:11:09 +0200

    do not show all characters inline

--- src/ui/pageObserver.ts
+++ src/ui/pageObserver.ts
@@ -1,7 +1,7 @@
 import { initAudioContext } from "../audio-crossfader";
 import { setCurrentLocation } from "../helpers/paragraphsNavigation";
 
-const SHOULD_SHOW_EVERYONE = true;
+const SHOULD_SHOW_EVERYONE = false;
 
 // --- Helper Functions ---
 
