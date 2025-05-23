372e41bb456bc1e30a6a4778d79aa6ced96f692a
Dominik Lasek <dominiklasek95@gmail.com>
2025-05-23 10:06:57 +0200

    adds VITE_EDITOR env and asks to confirm before changing content

--- src/hooks/useBookContent.ts
+++ src/hooks/useBookContent.ts
@@ -8,29 +8,35 @@ export function useBookContent(htmlContent: string, containerId: string) {
       console.warn(`Container with id '${containerId}' not found for content injection.`);
     }
 
-    const handleClick = async (event: MouseEvent) => {
-      const target = event.target as HTMLElement;
-      if (event.metaKey && target.tagName === "P") {
-        // Handle cmd + click on paragraph
-        event.preventDefault();
-        event.stopPropagation();
+    if (import.meta.env.VITE_EDITOR === "true") {
+      const handleClick = async (event: MouseEvent) => {
+        const target = event.target as HTMLElement;
+        if (event.metaKey) {
+          // Handle cmd + click on paragraph
+          event.preventDefault();
+          event.stopPropagation();
 
-        const chapter = (target.parentNode as HTMLElement).attributes["data-chapter"].value;
-        const paragraph = target.attributes["data-index"].value;
+          const chapter = (target.parentNode as HTMLElement).attributes["data-chapter"].value;
+          const paragraph = target.attributes["data-index"].value;
 
-        await fetch(`http://localhost:3000/api/text-editor/character`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ chapterNumber: chapter, paragraphNumber: paragraph, updatedParagraphText: `ss` }),
-        });
-      }
-    };
+          const response = confirm("Are you sure you want to edit this content?");
 
-    container?.addEventListener("click", handleClick);
+          if (!response) return;
 
-    // Clean up the event listener when the component unmounts or when dependencies change
-    return () => {
-      container?.removeEventListener("click", handleClick);
-    };
+          await fetch(`http://localhost:3000/api/text-editor/character`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ chapterNumber: chapter, paragraphNumber: paragraph, updatedParagraphText: `ss` }),
+          });
+        }
+      };
+
+      container?.addEventListener("click", handleClick);
+
+      // Clean up the event listener when the component unmounts or when dependencies change
+      return () => {
+        container?.removeEventListener("click", handleClick);
+      };
+    }
   }, [htmlContent, containerId]); // Rerun if content or ID changes
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_BOOK: string;
+  readonly VITE_EDITOR: string;
 }
 
 interface ImportMeta {
