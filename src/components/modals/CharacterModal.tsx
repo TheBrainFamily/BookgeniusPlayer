2fe883df82f0e81feee29f0e247396a05b73cbe2
dStojaniuk <stojaniuk.damian@gmail.com>
2025-05-23 11:18:46 +0200

    Hide modal on background click

--- src/components/modals/CharacterModal.tsx
+++ src/components/modals/CharacterModal.tsx
@@ -18,8 +18,8 @@ export const findLatestSummaryInRange = (character: CharacterData, endChapter: n
 
 const CharacterModal: React.FC<CharacterModalProps> = ({ onClose, isVideo, mediaSrc, matchingCharacter, endChapter }) => {
   return (
-    <ModalUI onClose={onClose} className="bg-transparent">
-      <div className="flex flex-col md:flex-row lg:flex-col gap-4 items-center">
+    <ModalUI onClose={onClose} className="bg-transparent pointer-events-none">
+      <div className="flex flex-col md:flex-row lg:flex-col gap-4 items-center pointer-events-none">
         <div className="rounded-full overflow-hidden h-full w-full max-h-[90vh] max-w-[90vh] lg:max-h-120 lg:max-w-120 border-4 border-[var(--entity-highlight-border-light)] aspect-square">
           <CharacterMedia
             mediaSrc={mediaSrc}
@@ -33,7 +33,7 @@ const CharacterModal: React.FC<CharacterModalProps> = ({ onClose, isVideo, media
             }}
           />
         </div>
-        <div className="flex flex-col self-center p-4 rounded-lg bg-[var(--entity-highlight-bg-light)] border-2 border-[var(--entity-highlight-border-light)] max-w-2xl">
+        <div className="flex flex-col self-center p-4 rounded-lg bg-[var(--entity-highlight-bg-light)] border-2 border-[var(--entity-highlight-border-light)] max-w-2xl pointer-events-auto">
           <h4 className="italic font-bold text-center">{matchingCharacter.characterName}</h4>
           <p className="text-center" dangerouslySetInnerHTML={{ __html: findLatestSummaryInRange(matchingCharacter, endChapter) || "" }} />
         </div>
