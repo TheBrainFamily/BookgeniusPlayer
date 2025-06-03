b3a50909076649d7b5bce35bfa6643e7d4503cfc
Tomasz Gierczyński <tgierczynski@thebrain.pro>
2025-06-03 09:26:01 +0200

    Add chapter data for '1984' and integrate into book data

--- src/booksData/books/1984-EnglishBookData.ts
+++ src/booksData/books/1984-EnglishBookData.ts
@@ -2,6 +2,7 @@ import { BookData } from "../types";
 import { getBookThemeColors } from "../bookThemes";
 import { BOOK_SLUGS } from "@/consts";
 import { _1984EnglishCharactersData } from "@/data/metadata-1984-English";
+import { _1984EnglishBookXml } from "@/data/chapters-1984-english";
 
 export const bookData: BookData = {
   slug: BOOK_SLUGS._1984_English,
@@ -9,5 +10,6 @@ export const bookData: BookData = {
   charactersData: _1984EnglishCharactersData,
   chapters: 3,
   themeColors: getBookThemeColors(BOOK_SLUGS._1984_English),
+  bookStringified: _1984EnglishBookXml,
   hasAudiobook: true,
 };
