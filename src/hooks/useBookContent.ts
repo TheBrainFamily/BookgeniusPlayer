09df18a123097760b84bc8d9950a6d5d9c6ab836
Lukasz Gandecki <lgandecki@css.edu>
2025-06-23 09:03:29 +0200

    replace sentence in line for now, not fully functional yet

--- src/hooks/useBookContent.ts
+++ src/hooks/useBookContent.ts
@@ -2,14 +2,16 @@ import { useEffect } from "react";
 import { useCharacterModal } from "@/stores/modals/characterModal.store";
 import { setupPageObserver } from "@/ui/pageObserver";
 import { getBookStringified } from "@/genericBookDataGetters/getBookStringified";
-import { useSentenceModal } from "@/stores/modals/sentenceModal.store";
+// import { useSentenceModal } from "@/stores/modals/sentenceModal.store";
 import { findSimplifiedSentence } from "@/helpers/findSimplifiedSentence";
+import { replaceXmlTagsIntoHtmlTags } from "@/helpers/replaceXmlTagsIntoHtmlTags";
+import { activateCharacterInteractions } from "@/helpers/activateCharacterInteractions";
 
 export function useBookContent(containerId: string) {
   const bookStringified = getBookStringified();
 
   const { openModal: openCharacterDetailsModal } = useCharacterModal();
-  const { openModal: openSentenceModal } = useSentenceModal();
+  // const { openModal: openSentenceModal } = useSentenceModal();
 
   useEffect(() => {
     const container = document.getElementById(containerId);
@@ -21,10 +23,21 @@ export function useBookContent(containerId: string) {
         const target = event.target as HTMLInputElement;
         if (target.tagName === "SPAN" && /^ch\d+-p\d+-s\d+$/.test(target.id)) {
           const currentSentenceId = target.id;
-          const currentSentence = target.textContent;
+          // const currentSentence = target.textContent;
           const currentSentenceScore = target.getAttribute("data-current-score");
           const { text: simplifiedSentence, score: simplifiedSentenceScore } = findSimplifiedSentence(target.id, parseInt(currentSentenceScore));
-          openSentenceModal(currentSentence, simplifiedSentence, currentSentenceId, simplifiedSentenceScore);
+          if (!simplifiedSentence) {
+            console.warn(`Simplified sentence not found for ${currentSentenceId}`);
+            return;
+          }
+          target.innerHTML = replaceXmlTagsIntoHtmlTags(simplifiedSentence);
+
+          // 4. Update the state on the element!
+          target.dataset.currentScore = simplifiedSentenceScore.toString();
+
+          // 5. Activate character interactions for newly transformed content
+          activateCharacterInteractions(target, openCharacterDetailsModal);
+          // openSentenceModal(currentSentence, simplifiedSentence, currentSentenceId, simplifiedSentenceScore);
         }
       });
     } else {
