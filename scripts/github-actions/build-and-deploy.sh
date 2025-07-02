774e603e42619570b97b12625ffef156fa957579
Daniel-Strak_1701 <lucetius@lucetius.pl>
2025-07-02 10:42:53 +0200

    run deployments in parallel

--- scripts/github-actions/build-and-deploy.sh
+++ scripts/github-actions/build-and-deploy.sh
@@ -1,6 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+BOOKS_DIR="public_books"
+
+if [ -z "$1" ]; then
+  echo "You need to pass title"
+  exit 1
+fi
+
+BOOK_NAME="$1"
+BOOK_PATH="$BOOKS_DIR/$BOOK_NAME"
+
+if [ ! -d "$BOOK_PATH" ]; then
+  echo "Book directory not found $BOOK_PATH"
+  exit 1
+fi
+
+if [ -z "$DEPLOY_HOST" ]; then
+  echo "Environment variable DEPLOY_HOST is not set."
+  exit 1
+fi
 
 # Different locales for specific books
 get_book_lang() {
@@ -13,53 +33,36 @@ get_book_lang() {
 # Different subdomain/directory for specific books
 get_deploy_dir() {
   case "$1" in
-    "Alice-Wonderland") echo "alice" ;;
-    "Conrad-Tajny-Agent") echo "tajny-agent" ;;
+#    "Alice-Wonderland") echo "alice" ;;
+#    "Conrad-Tajny-Agent") echo "tajny-agent" ;;
     *) echo "" ;;
   esac
 }
-BOOKS_DIR="public_books"
 
-if [ -z "$DEPLOY_HOST" ]; then
-  echo "Environment variable DEPLOY_HOST is not set."
-  exit 1
-fi
+VITE_LANG=$(get_book_lang "$BOOK_NAME")
+DEPLOY_DIR=$(get_deploy_dir "$BOOK_NAME")
 
-echo "Cleaning up ./dist"
-rm -rf ./dist
-
-for dir in "$BOOKS_DIR"/*/; do
-  BOOK_NAME=$(basename "$dir")
-  VITE_LANG=$(get_book_lang "$BOOK_NAME")
-
-  if [[ -n "${BOOK_LANG_MAP[$BOOK_NAME]}" ]]; then
-    VITE_LANG="${BOOK_LANG_MAP[$BOOK_NAME]}"
-  fi
-
-  VITE_BOOK="$BOOK_NAME"
-  VITE_BOOK_PATH="$BOOKS_DIR/$BOOK_NAME/"
-  VITE_BOOK_DIR="$BOOKS_DIR/$BOOK_NAME"
-
-  echo "Building and syncing book: $BOOK_NAME (lang=$VITE_LANG)"
-
-  pnpm build "$VITE_BOOK_DIR"
-
-  VITE_LANG="$VITE_LANG" \
-  VITE_BOOK="$VITE_BOOK" \
-  VITE_BOOK_NAME="$VITE_BOOK" \
-  VITE_BOOK_PATH="$VITE_BOOK_PATH" \
-  VITE_BOOK_DIR="$VITE_BOOK_DIR" \
-  npx vite build
-
-  DEPLOY_DIR=$(get_deploy_dir "$BOOK_NAME")
-  if [ -n "$DEPLOY_DIR" ]; then
+if [ -n "$DEPLOY_DIR" ]; then
   LOWERCASE_BOOK_NAME="$DEPLOY_DIR"
 else
   LOWERCASE_BOOK_NAME=$(echo "$BOOK_NAME" | tr '[:upper:]' '[:lower:]' | sed 's/[-_]//g')
 fi
 
-  rsync -av ./dist/ root@"$DEPLOY_HOST":/var/www/"$LOWERCASE_BOOK_NAME"
+echo "Target server directory: $LOWERCASE_BOOK_NAME"
 
-  echo "Cleaning up ./dist"
-  rm -rf ./dist
-done
+rm -rf ./dist
+
+#VITE_LANG="$VITE_LANG" \
+#VITE_BOOK="$BOOK_NAME" \
+#VITE_BOOK_NAME="$BOOK_NAME" \
+#VITE_BOOK_PATH="$BOOK_PATH/" \
+#VITE_BOOK_DIR="$BOOK_PATH" \
+#npx vite build
+
+pnpm build "$BOOK_PATH"
+
+echo "Sending: $DEPLOY_HOST:/var/www/$LOWERCASE_BOOK_NAME"
+rsync -av ./dist/ root@"$DEPLOY_HOST":/var/www/daniel-"$LOWERCASE_BOOK_NAME"
+
+rm -rf ./dist
+echo "Done."
