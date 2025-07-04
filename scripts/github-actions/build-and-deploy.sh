3201a9a43d813a9d68c72aae974288ae7dacd114
Daniel-Strak_1701 <lucetius@lucetius.pl>
2025-07-04 13:09:35 +0200

    tryting to restore pointer for changed files

--- scripts/github-actions/build-and-deploy.sh
+++ scripts/github-actions/build-and-deploy.sh
@@ -69,12 +69,14 @@ fi
 ARCHIVE_NAME="${BOOK_NAME}.tar.gz"
 S3_REMOTE_PATH="s3://${DEPLOY_AWS_BUCKET}/main/${ARCHIVE_NAME}"
 if [[ "$BRANCH_NAME" != "main" ]]; then
+  echo "dir size"
+  du -kh -d 1 public_books
   echo "file content before fetching"
   cat public_books/Lalka/assets/adwokat.png | head -n 1
   echo "*****"
   export GIT_LFS_SKIP_SMUDGE=1
-  GIT_LFS_SKIP_SMUDGE=1 git lfs install --skip-repo
-  GIT_LFS_SKIP_SMUDGE=1 git fetch origin main --depth=1
+  GIT_LFS_SKIP_SMUDGE=1 GIT_TRACE=1 git lfs install --skip-repo
+  GIT_LFS_SKIP_SMUDGE=1 GIT_TRACE=1 git fetch origin main --depth=1
   BASE_SHA=$(git rev-parse origin/main)
   CHANGED_FILES=$(git diff --name-only ${BASE_SHA} HEAD -- public public_books || true)
 
