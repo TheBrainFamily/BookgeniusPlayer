a64818a2e384865b0da4622e800f4a16108cf1a6
Daniel-Strak_1701 <lucetius@lucetius.pl>
2025-07-15 12:11:17 +0200

    fix chat style layout when there are two characters speaking in same time

--- scripts/data/xmlToComplexHtml.ts
+++ scripts/data/xmlToComplexHtml.ts
@@ -92,6 +92,7 @@ export const xmlToComplexHtml = (
   let currentAct = "";
   let currentCharacterAlignment = "";
   let isCharacter = false;
+  let lastSpanId = "";
 
   for (const chapter of chapters) {
     const chapterId = chapter.getAttribute("id");
@@ -191,11 +192,14 @@ export const xmlToComplexHtml = (
                       isCharacter = true;
                       const startOfParagraphClass = !hasSignificantTextContent ? " start-of-paragraph" : "";
                       inner += `<span class="character-placeholder character-talking${startOfParagraphClass}" data-character="${slug}" data-src-talking="${talkingSrc}" data-is-talking="true"></span>`;
-                      if (!currentCharacterAlignment || currentCharacterAlignment === "right") {
-                        currentCharacterAlignment = "left";
-                      } else {
-                        currentCharacterAlignment = "right";
+                      if (spanId !== lastSpanId) {
+                        if (!currentCharacterAlignment || currentCharacterAlignment === "right") {
+                          currentCharacterAlignment = "left";
+                        } else {
+                          currentCharacterAlignment = "right";
+                        }
                       }
+                      lastSpanId = spanId;
                     } else {
                       if (e.getAttribute("dynasty") === "true") {
                         inner += e.textContent;
@@ -231,7 +235,6 @@ export const xmlToComplexHtml = (
               pContent += `<span id="${spanId}">${inner}</span>`;
               continue;
             }
-
             // 2b) character‐tag (e.g. <Alice>)
             const ci = characterMap.get(pElement.tagName);
             if (ci) {
