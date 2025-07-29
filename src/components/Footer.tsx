607a98314557d2423bd076b0830e2b39d358acfe
dStojaniuk <stojaniuk.damian@gmail.com>
2025-07-29 12:08:15 +0200

    Do not set pixels directly, set the width using percentages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,7 +28,7 @@ const Footer = () => {
           className={cn("fixed bottom-0 inset-x-0 z-50 flex flex-row gap-2 justify-center mx-auto max-w-[120rem] w-full pointer-events-none", "px-2 py-3 sm:pr-0 xl:px-4 sm:pl-4")}
         >
           <div id="left-notes-blank" className="hidden sm:block sm:flex-1 max-w-[700px]" />
-          <div className="flex-2 sm:min-w-[500px] md:min-w-[600px] max-w-[900px] px-0 flex flex-col ml-0 sm:px-3 xl:px-2 space-y-3 pointer-events-auto">
+          <div className="w-full sm:flex-3 max-w-[900px] px-0 flex flex-col sm:ml-2 sm:px-3 xl:ml-0 xl:px-2 space-y-3 pointer-events-auto">
             <BottomInput onSubmit={sendMessage} />
           </div>
           {!isRightNotesBlankHidden && <div id="right-notes-blank" className="hidden xl:block xl:flex-1 max-w-[700px]" />}
