2e5efc3493ebeecc00d1381d6fe3292f5c5ab9cf
Dominik Lasek <dominiklasek95@gmail.com>
2025-08-14 11:56:36 +0200

    adds defensive guard

--- apps/player/src/helpers/addPaddingBottomLastChapter.ts
+++ apps/player/src/helpers/addPaddingBottomLastChapter.ts
@@ -1,4 +1,6 @@
 export const addPaddingBottomLastChapter = (doc: Document, chapters: Element[]) => {
+  if (chapters.length === 0) return;
+
   const lastChapter = chapters[chapters.length - 1];
   const finalPadding = doc.createElement("div");
   finalPadding.style.height = "50vh";
