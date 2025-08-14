c6b96eb1fd44a00f2bc6cc891f70518bce602274
Przemysław Gierski <pgierski@o2.pl>
2025-08-14 16:21:35 +0200

    use characters list for new logic

--- apps/player/src/searchModal.ts
+++ apps/player/src/searchModal.ts
@@ -116,6 +116,10 @@ const createContextualSummary = (fullText: string, query: string, maxLength: num
 export function performCachedSearch(query: string, currentLocation: Location): SearchResultsData {
   const { textCache, isInitialized } = useBookContentStore.getState();
   const bookIsPlay = getBookData().metadata.bookForm === "play";
+  let bookCharacters = [];
+  if (bookIsPlay) {
+    bookCharacters = getCharactersData().map((character) => character.characterName.toLowerCase());
+  }
 
   const items: SearchResultItemData[] = [];
   const queryLower = query.toLowerCase();
@@ -147,7 +151,7 @@ export function performCachedSearch(query: string, currentLocation: Location): S
       if (paragraphText.toLowerCase().includes(queryLower)) {
         if (bookIsPlay) {
           // if the paragraph text has just 1 word we want to append the next paragraph text to show the character next line
-          if (!paragraphText.trim().includes(" ")) {
+          if (bookCharacters.includes(paragraphText.trim().toLowerCase())) {
             const nextParagraphText = chapterCache[paragraphNumber + 1];
 
             if (nextParagraphText) {
