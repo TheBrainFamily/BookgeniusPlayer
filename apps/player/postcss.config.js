ac6085f6c908bbb7af40b4e8aebbdccc94a6029c
dStojaniuk <stojaniuk.damian@gmail.com>
2025-08-16 10:58:33 +0200

    config cleanup

--- apps/player/postcss.config.js
+++ apps/player/postcss.config.js
@@ -1,4 +1,5 @@
-export default {
+/* global module */
+module.exports = {
   plugins: {
     "@tailwindcss/postcss": {}, // Tells PostCSS to use the Tailwind plugin
     "postcss-nesting": {}, // Enables nesting of CSS rules, similar to Sass
