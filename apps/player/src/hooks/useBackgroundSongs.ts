0e51a50084fe6ceb7c929e666815ce30b52777f1
Przemysław Gierski <pgierski@o2.pl>
2025-08-17 20:57:06 +0200

    remove console log

--- apps/player/src/hooks/useBackgroundSongs.ts
+++ apps/player/src/hooks/useBackgroundSongs.ts
@@ -43,7 +43,6 @@ export function useBackgroundSongs() {
 
   useEffect(() => {
     if (!isAppReady) return;
-    console.log("PINGWING, preloadCurrentTrack");
     preloadCurrentTrack();
   }, [isAppReady]);
 
