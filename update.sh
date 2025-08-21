5228131ef1ab8ab08595e82154817dbcb22907a0
Lukasz Gandecki <lgandecki@css.edu>
2025-08-21 12:01:11 +0200

    proper branch context for s5cmd upload

--- update.sh
+++ update.sh
@@ -28,10 +28,10 @@ aws s3 sync build/player-app/ s3://$S3_BUCKET/app/player/${ASSET_CONTEXT}/ \
   --only-show-errors --size-only --no-progress --delete
 
 if [[ "$*" == *"--with-books"* ]]; then
-  s5cmd --numworkers 256 cp -c 256 --cache-control "public, max-age=31536000, immutable"  build/s3-data/assets/ "s3://$S3_BUCKET/$CTX/assets/"
+  s5cmd --numworkers 256 cp -c 256 --cache-control "public, max-age=31536000, immutable"  build/s3-data/assets/ "s3://${S3_BUCKET}/${ASSET_CONTEXT}/assets/"
   #s5cmd --numworkers=256 cp --show-progress build/s3-data/assets/ "s3://$S3_BUCKET/$CTX/assets/" --cache-control "public, max-age=31536000, immutable" 
 
-  aws s3 cp build/s3-data/versions.json s3://$S3_BUCKET/$CTX/versions.json \
+  aws s3 cp build/s3-data/versions.json "s3://${S3_BUCKET}/${ASSET_CONTEXT}/versions.json" \
     --content-type 'application/json; charset=utf-8' \
     --cache-control 'public, max-age=60, s-maxage=300, stale-while-revalidate=86400' 
 fi
