d798ce5ae9eaa9d911416d5787cc7f1395ee3ca7
dStojaniuk <stojaniuk.damian@gmail.com>
2025-08-29 11:28:16 +0200

    remove unneeded export

--- apps/player/src/i18n.ts
+++ apps/player/src/i18n.ts
@@ -3,7 +3,7 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import resourcesToBackend from "i18next-resources-to-backend";
 
-export const i18n: I18nInstance = i18next.createInstance();
+const i18n: I18nInstance = i18next.createInstance();
 
 i18n
   .use(LanguageDetector)
