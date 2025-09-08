f4226ec652a75c353e9a43879e373898134df0cd
dStojaniuk <stojaniuk.damian@gmail.com>
2025-09-08 12:23:30 +0200

    add more space around ui elements on larger screen

--- apps/player/src/components/Header.tsx
+++ apps/player/src/components/Header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
           variants={headerVariants}
           initial="hidden"
           animate="visible"
-          className={cn("fixed top-0 inset-x-0 z-50 flex items-center max-w-[120rem] w-full mx-auto", "gap-1 md:gap-2 px-2 pt-4")}
+          className={cn("fixed top-0 inset-x-0 z-50 flex items-center max-w-[120rem] w-full mx-auto", "gap-1 md:gap-2 px-2 lg:px-4 pt-4")}
         >
           <MenuButton />
           <AudioPlayer />
