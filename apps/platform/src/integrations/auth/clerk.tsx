d4be81ed6b1be01162d81b51a70dbde738c94a9c
Daniel-Strak_1701 <lucetius@lucetius.pl>
2025-09-08 11:56:52 +0200

    refactor after review

--- apps/platform/src/integrations/auth/clerk.tsx
+++ apps/platform/src/integrations/auth/clerk.tsx
@@ -130,7 +130,7 @@ const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => 
 
   const signInUrlEnv = import.meta.env.VITE_PUBLIC_CLERK_SIGN_IN_URL;
   const signUpUrlEnv = import.meta.env.VITE_PUBLIC_CLERK_SIGN_UP_URL;
-  if (isSatellite && signInUrlEnv && signUpUrlEnv) {
+  if (isSatellite && signInUrlEnv && signUpUrlEnv && clerkDomain) {
     const clerkDomainUrl = `https://${clerkDomain}`;
     const signInUrlObj = new URL(signInUrlEnv);
     signInUrlObj.searchParams.append('redirect_url', clerkDomainUrl);
