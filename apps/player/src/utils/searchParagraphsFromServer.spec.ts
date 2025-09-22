e2ed4971ae151db0a20d41d9657fc0dc04473cfa
Lukasz Gandecki <lgandecki@css.edu>
2025-09-22 12:48:12 +0200

    improved embedding search

--- apps/player/src/utils/searchParagraphsFromServer.spec.ts
+++ apps/player/src/utils/searchParagraphsFromServer.spec.ts
@@ -3,8 +3,8 @@ import { parseSearchParagraphsServerResponse, SearchParagraphsServerResponse } f
 
 test("parseSearchParagraphsServerResponse", () => {
   const response: SearchParagraphsServerResponse[] = [
-    { chapter: 1, paragraphNumber: 1, text: "<Summary>Sara próbuje pocieszyć Ramzesa.</Summary> <Text>W tej chwili usłyszał między drzewami cichy szelest</Text>" },
+    { chapter: 1, paragraphNumber: 1, text: "<Summary>Sara próbuje pocieszyć Ramzesa.</Summary> <Text>W tej chwili usłyszał między drzewami cichy szelest</Text>", score: 0.9 },
   ];
   const result = parseSearchParagraphsServerResponse(response);
-  expect(result).toEqual([{ chapter: 1, paragraphNumber: 1, text: "W tej chwili usłyszał między drzewami cichy szelest", summary: "Sara próbuje pocieszyć Ramzesa." }]);
+  expect(result).toEqual([{ chapter: 1, paragraphNumber: 1, text: "W tej chwili usłyszał między drzewami cichy szelest", summary: "Sara próbuje pocieszyć Ramzesa.", score: 0.9 }]);
 });
