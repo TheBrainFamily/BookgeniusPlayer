df208b4c75e5a1172b70ece61cc90fac531fcca2
Lukasz Gandecki <lgandecki@css.edu>
2025-09-23 14:40:43 +0200

    allow deployment

--- apps/wukong/postcss.config.js
+++ apps/wukong/postcss.config.js
@@ -1,1 +1,6 @@
-export default { plugins: { tailwindcss: {}, autoprefixer: {} } };
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {}, // Tells PostCSS to use the Tailwind plugin
+    "postcss-nesting": {}, // Enables nesting of CSS rules, similar to Sass
+  },
+};
