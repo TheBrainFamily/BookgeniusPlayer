a625f10d2b75ed2ae2c04a5dfd4cc631aec598a2
dStojaniuk <stojaniuk.damian@gmail.com>
2025-09-23 15:33:51 +0200

    fix styles for wukong platform

--- apps/wukong/vite.config.ts
+++ apps/wukong/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import tailwind from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: { host: "::", port: 8080 },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger(), tailwind()].filter(Boolean),
   resolve: { alias: { "@wukong": path.resolve(__dirname, "./src"), "@platform": path.resolve(__dirname, "../platform/src"), "@player": path.resolve(__dirname, "../player/src") } },
 }));
