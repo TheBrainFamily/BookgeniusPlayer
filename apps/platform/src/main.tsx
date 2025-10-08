20964124e34fdf8b76f41b0e59cb216e9d571cdd
Łukasz Gandecki <4002543+lgandecki@users.noreply.github.com>
2025-10-08 14:03:09 +0200

    frontend sentry integration (#666)
    * frontend sentry integration
    
    * fix pnpm-lock
    
    * attempt to trigger sentry error in deployed
    
    * further integrate sentry with the app
    
    * book colors added so no 404 errors

--- apps/platform/src/main.tsx
+++ apps/platform/src/main.tsx
@@ -1,7 +1,8 @@
+import "./sentry";
 import { ViteReactSSG } from "vite-react-ssg/single-page";
 import App from "./App.tsx";
 
 import "./styles/index.css";
-import "./i18n";
+import "./i18n/index.ts";
 
 export const createRoot = ViteReactSSG(<App />);
