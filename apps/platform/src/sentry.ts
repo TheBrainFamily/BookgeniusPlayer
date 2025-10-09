f6586ee2f3633bcca3428867a85759e5d5da4756
Dominik Lasek <dominiklasek95@gmail.com>
2025-10-09 16:35:42 +0200

    removes logs

--- apps/platform/src/sentry.ts
+++ apps/platform/src/sentry.ts
@@ -1,11 +1,6 @@
 import * as Sentry from "@sentry/react";
 
 console.log("loading sentry");
-
-console.log("5: process.env.NODE_ENV BANG!", process.env.NODE_ENV);
-console.log("6: import.meta.env.MODE BANG!", import.meta.env.MODE);
-console.log("7: import.meta.env.PROD BANG!", import.meta.env.PROD);
-
 Sentry.init({
   dsn: "https://ec8e06caac85f65ed3bab3efcb45d88e@o4510025358311424.ingest.de.sentry.io/4510152806039632",
   integrations: [
