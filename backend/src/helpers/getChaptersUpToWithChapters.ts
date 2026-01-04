465d9822d6a0ad44d66203db08e3a819b843a5c8
Lukasz Gandecki <lgandecki@css.edu>
2026-01-04 17:25:55 +0100

    cleanup

--- backend/src/helpers/getChaptersUpToWithChapters.ts
+++ backend/src/helpers/getChaptersUpToWithChapters.ts
@@ -2,11 +2,8 @@ import { Chapter } from "../types";
 import * as cheerio from "cheerio";
 
 export const getChaptersUpToWithChapters = (from: number, upTo: number, chapters: string) => {
-  // Parse chapters XML using cheerio
   const $ = cheerio.load(chapters, { xml: true });
 
-  // console.log("chapters", chapters);
-  // Extract all chapters
   const parsedChapters: Chapter[] = [];
   $("chapter").each((_, element) => {
     const $element = $(element);
@@ -16,14 +13,6 @@ export const getChaptersUpToWithChapters = (from: number, upTo: number, chapters
 
     parsedChapters.push({ number, title, content });
   });
-  console.log(
-    "\n\n\nparsedChapters",
-    parsedChapters.map((chapter) => chapter.number),
-  );
 
-  console.log("from", from);
-  console.log("upTo", upTo);
-
-  // Filter chapters based on the provided range
   return parsedChapters.filter((chapter) => chapter.number >= from && chapter.number <= upTo);
 };
